54c3b19a7fcd2abc6d4ee8fe81bcd17f7c3a4f41
Amit Gupta <akaamitgupta@gmail.com>
2017-02-02 00:01:22 +0530

    Adding Webpack and Modularizing HTTP Requests

--- js/app.js
+++ js/app.js
@@ -1,24 +1,4 @@
-// XMLHttpRequest wrapper using callbacks
-let request = obj => {
-	return new Promise((resolve, reject) => {
-		let xhr = new XMLHttpRequest();
-		xhr.open(obj.method || "GET", obj.url);
-		if (obj.headers) {
-			Object.keys(obj.headers).forEach(key => {
-				xhr.setRequestHeader(key, obj.headers[key]);
-			});
-		}
-		xhr.onload = () => {
-			if (xhr.status >= 200 && xhr.status < 300) {
-				resolve(xhr.response);
-			} else {
-				reject(xhr.statusText);
-			}
-		};
-		xhr.onerror = () => reject(xhr.statusText);
-		xhr.send(obj.body);
-	});
-};
+import request from './request';
 
 let token = '4527699633.3f66c67.c099167d0a8943a8a222183a65c1c150';
 let hashtag = 'party';
