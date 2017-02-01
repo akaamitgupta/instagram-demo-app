803f55ff85746b3f7f66c141200d35f802ff9814
Amit Gupta <akaamitgupta@gmail.com>
2017-02-01 23:50:21 +0530

    Promisifying XMLHttpRequest

--- js/app.js
+++ js/app.js
@@ -1,32 +1,32 @@
 // XMLHttpRequest wrapper using callbacks
-let request = (obj, successHandler, errorHandler) => {
-	let xhr = new XMLHttpRequest();
-	xhr.open(obj.method || "GET", obj.url);
-	if (obj.headers) {
-		Object.keys(obj.headers).forEach((key) => {
-			xhr.setRequestHeader(key, obj.headers[key]);
-		});
-	}
-	xhr.onload = () => {
-		if (xhr.status >= 200 && xhr.status < 300) {
-			successHandler(xhr.response);
-		} else {
-			errorHandler(xhr.statusText);
+let request = obj => {
+	return new Promise((resolve, reject) => {
+		let xhr = new XMLHttpRequest();
+		xhr.open(obj.method || "GET", obj.url);
+		if (obj.headers) {
+			Object.keys(obj.headers).forEach(key => {
+				xhr.setRequestHeader(key, obj.headers[key]);
+			});
 		}
-	};
-	xhr.onerror = () => {
-		errorHandler(xhr.statusText);
-	};
-	xhr.send(obj.body);
-}
+		xhr.onload = () => {
+			if (xhr.status >= 200 && xhr.status < 300) {
+				resolve(xhr.response);
+			} else {
+				reject(xhr.statusText);
+			}
+		};
+		xhr.onerror = () => reject(xhr.statusText);
+		xhr.send(obj.body);
+	});
+};
 
 let token = '4527699633.3f66c67.c099167d0a8943a8a222183a65c1c150';
 let hashtag = 'party';
 let limit = 10;
 
 // Self
-request({url: `https://api.instagram.com/v1/users/self/media/recent?access_token=${token}&count=${limit}`},
-	(data) => {
+request({url: `https://api.instagram.com/v1/users/self/media/recent?access_token=${token}&count=${limit}`})
+	.then(data => {
 		data = JSON.parse(data);
 		console.log(data);
 		let html = "";
@@ -38,15 +38,14 @@ request({url: `https://api.instagram.com/v1/users/self/media/recent?access_token
 			`;
 		});
 		document.getElementById('instagram-self').innerHTML = html;
-	},
-	(error) => {
+	})
+	.catch(error => {
 		console.log(error);
-	}
-);
+	});
 
 // By a #hashtag
-request({url: `https://api.instagram.com/v1/tags/${hashtag}/media/recent?access_token=${token}&count=${limit}`},
-	(data) => {
+request({url: `https://api.instagram.com/v1/tags/${hashtag}/media/recent?access_token=${token}&count=${limit}`})
+	.then(data => {
 		data = JSON.parse(data);
 		console.log(data);
 		let html = "";
@@ -58,8 +57,7 @@ request({url: `https://api.instagram.com/v1/tags/${hashtag}/media/recent?access_
 			`;
 		});
 		document.getElementById('instagram-hashtag').innerHTML = html;
-	},
-	(error) => {
+	})
+	.catch(error => {
 		console.log(error);
-	}
-);
+	});
