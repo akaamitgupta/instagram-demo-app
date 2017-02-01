a8fe78b9ef6dbab3aa871abb1aa5e3856aa2b899
Amit Gupta <akaamitgupta@gmail.com>
2017-02-02 00:20:48 +0530

    Creating a Data Service

--- js/app.js
+++ js/app.js
@@ -1,43 +1,54 @@
-import request from './request';
+import Instagram from './instagram';
 
 let token = '4527699633.3f66c67.c099167d0a8943a8a222183a65c1c150';
 let hashtag = 'party';
 let limit = 10;
 
 // Self
-request({url: `https://api.instagram.com/v1/users/self/media/recent?access_token=${token}&count=${limit}`})
+let instagram_self = new Instagram({
+    get: 'self',
+    limit: 10,
+    accessToken: token
+});
+
+instagram_self.run()
 	.then(data => {
-		data = JSON.parse(data);
-		console.log(data);
-		let html = "";
-		data.data.forEach((insta) => {
-			html += `
-				<a href="${insta.link}">
-					<img src="${insta.images.low_resolution.url}">
-				</a>
-			`;
-		});
-		document.getElementById('instagram-self').innerHTML = html;
+	    console.log(data);
+	    let html = "";
+	    data.data.forEach((ph) => {
+	        html += `
+	            <a href="${ph.link}">
+	                <img src="${ph.images.low_resolution.url}">
+	            </a>
+	        `;
+	    });
+	    document.getElementById('instagram-self').innerHTML = html;
 	})
 	.catch(error => {
-		console.log(error);
+	    console.log(error);
 	});
 
+
 // By a #hashtag
-request({url: `https://api.instagram.com/v1/tags/${hashtag}/media/recent?access_token=${token}&count=${limit}`})
+let instagram_hashtag = new Instagram({
+    get: 'tagged',
+    tagName: hashtag,
+    accessToken: token
+});
+
+instagram_hashtag.run()
 	.then(data => {
-		data = JSON.parse(data);
-		console.log(data);
-		let html = "";
-		data.data.forEach((insta) => {
-			html += `
-				<a href="${insta.link}">
-					<img src="${insta.images.low_resolution.url}">
-				</a>
-			`;
-		});
-		document.getElementById('instagram-hashtag').innerHTML = html;
+	    console.log(data);
+	    let html = "";
+	    data.data.forEach((ph) => {
+	        html += `
+	            <a href="${ph.link}">
+	                <img src="${ph.images.low_resolution.url}">
+	            </a>
+	        `;
+	    });
+	    document.getElementById('instagram-hashtag').innerHTML = html;
 	})
 	.catch(error => {
-		console.log(error);
+	    console.log(error);
 	});
