1de33b47500609e3db14dde0ab26296540b41724
Amit Gupta <akaamitgupta@gmail.com>
2017-02-01 23:41:08 +0530

    ES6 and babel added

--- js/app.js
+++ js/app.js
@@ -1,57 +1,65 @@
 // XMLHttpRequest wrapper using callbacks
-function request(obj, successHandler, errorHandler) {
-	var xhr = new XMLHttpRequest();
+let request = (obj, successHandler, errorHandler) => {
+	let xhr = new XMLHttpRequest();
 	xhr.open(obj.method || "GET", obj.url);
 	if (obj.headers) {
-		Object.keys(obj.headers).forEach(function(key) {
+		Object.keys(obj.headers).forEach((key) => {
 			xhr.setRequestHeader(key, obj.headers[key]);
 		});
 	}
-	xhr.onload = function() {
+	xhr.onload = () => {
 		if (xhr.status >= 200 && xhr.status < 300) {
 			successHandler(xhr.response);
 		} else {
 			errorHandler(xhr.statusText);
 		}
 	};
-	xhr.onerror = function() {
+	xhr.onerror = () => {
 		errorHandler(xhr.statusText);
 	};
 	xhr.send(obj.body);
 }
 
-var token = '4527699633.3f66c67.c099167d0a8943a8a222183a65c1c150';
-var hashtag = 'party';
-var limit = 10;
+let token = '4527699633.3f66c67.c099167d0a8943a8a222183a65c1c150';
+let hashtag = 'party';
+let limit = 10;
 
 // Self
-request({url: 'https://api.instagram.com/v1/users/self/media/recent?access_token=' + token + '&count=' + limit},
-	function(data) {
-		var data = JSON.parse(data);
+request({url: `https://api.instagram.com/v1/users/self/media/recent?access_token=${token}&count=${limit}`},
+	(data) => {
+		data = JSON.parse(data);
 		console.log(data);
-		var html = "";
-		data.data.forEach(function(insta) {
-			html += '<a href="' + insta.link + '"><img src="' + insta.images.low_resolution.url + '"></a>';
+		let html = "";
+		data.data.forEach((insta) => {
+			html += `
+				<a href="${insta.link}">
+					<img src="${insta.images.low_resolution.url}">
+				</a>
+			`;
 		});
 		document.getElementById('instagram-self').innerHTML = html;
 	},
-	function(error) {
+	(error) => {
 		console.log(error);
 	}
 );
 
 // By a #hashtag
-request({url: 'https://api.instagram.com/v1/tags/' + hashtag + '/media/recent?access_token=' + token + '&count=' + limit},
-	function(data) {
-		var data = JSON.parse(data);
+request({url: `https://api.instagram.com/v1/tags/${hashtag}/media/recent?access_token=${token}&count=${limit}`},
+	(data) => {
+		data = JSON.parse(data);
 		console.log(data);
-		var html = "";
-		data.data.forEach(function(insta) {
-			html += '<a href="' + insta.link + '"><img src="' + insta.images.low_resolution.url + '"></a>';
+		let html = "";
+		data.data.forEach((insta) => {
+			html += `
+				<a href="${insta.link}">
+					<img src="${insta.images.low_resolution.url}">
+				</a>
+			`;
 		});
 		document.getElementById('instagram-hashtag').innerHTML = html;
 	},
-	function(error) {
+	(error) => {
 		console.log(error);
 	}
 );
